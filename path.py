49704a4ffff6bd2f7480054dc158f38a29df4c3a
Raimund Koop <rkoop@student.42heilbronn.de>
2022-06-23 10:11:26 +0200

    path

--- path.py
+++ path.py
@@ -1,5 +1,10 @@
+#marius
 img_path = '/Users/mawinter/Desktop/Bilder/'
-
 texture_path = '/Users/mawinter/Desktop/Bilder/'
 
+
+#ray
+img_path = '/Users/rkoop/Documents/cdvbw22/Data_Staatsgalerie_Stuttgart/Bilder/'
+texture_path = img_path
+#texture_path = '/Users/rkoop/Documents/cdvbw22/githubrepo/textures/'
 icon_path = '/Users/rkoop/Documents/cdvbw22/githubrepo/gui/icons/'
